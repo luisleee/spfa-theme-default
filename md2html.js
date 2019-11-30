a9b8b11e32db388a0c3a9c9cc0ccc0a8bd71711a
luisleee <luisleee@126.com>
2019-11-30 20:45:48 +0800

    v0.0.6

--- md2html.js
+++ md2html.js
@@ -11,61 +11,37 @@ marked.setOptions({
 const htmlSkeleton = `<!DOCTYPE html>
 <html>
 <head>
-<tile> __spfa_title__ </title>
+<title> __spfa_title__ </title>
 <meta charset="utf-8">
 </head>
 <body>
 <h1> __spfa_title__ </h1>
- __spfa_css__
+ __spfa_css__ 
  __spfa_body__ 
 </body>
 </html>`;
 
-var cssSkeleton = `<link rel="stylesheet" type="text/css" href=" __css_href__ ">`;
+var cssSkeleton = `<link rel="stylesheet" type="text/css" href=" __spfa_href__ ">`;
 
 function cssSection(href) {
-    return cssSkeleton.replace(" __spfa_css__ ", href);
+    return cssSkeleton.replace(/ __spfa_href__ /g, href);
 }
 
 function convert(string, title, style) {
-	var info = frontMatter(string);
-	console.log(info);
+    var info = frontMatter(string);
     var ititle = info.attributes.title;
     title = ititle ? ititle : title;
     string = info.body;
     var cssSegs = cssSection("../lib/spfa.css") + "\n";
-    cssSegs += "../lib/" + style + ".css\n";
+    cssSegs += cssSection("../lib/" + style + ".css") + "\n";
     var htmlBody = marked(string);
 
     var page = htmlSkeleton
-        .replace(" __spfa_css__ ", cssSegs)
-        .replace(" __spfa_body__ ", htmlBody)
-        .replace(" __spfa_title__ ", title);
+        .replace(/ __spfa_css__ /g, cssSegs)
+        .replace(/ __spfa_body__ /g, htmlBody)
+        .replace(/ __spfa_title__ /g, title);
+    return page;
 }
-
-//TODO: finish generating
-
-/* var header1 = "<!DOCTYPE html><html><head><title>";
-var header2 = "</title>";
-var header3 = "<meta charset='utf-8'></head><body>";
-var back = "</body></html>";
-
-var cssfront = "<link rel='stylesheet' type='text/css' href='";
-var cssback = "'>";
-
-function wrap(front, inner, back) {
-    return front + inner + back;
-}
-
-function convert(string, title, style) {
-    var data = wrap(header1, title, header2);
-
-    data += wrap(cssfront, "../lib/" + "spfa" + ".css", cssback);
-    data += wrap(cssfront, "../lib/" + style + ".css", cssback);
-    data += wrap(header3, marked(string), back);
-
-    return data;
-} */
 
 module.exports.md2html = function(from, to, title, theme) {
     console.log(from);
